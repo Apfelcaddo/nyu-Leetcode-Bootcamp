2a8239ed489058538deacf24cd93cab9d41a448d
sienamm <siyuan.ma77@gmail.com>
2024-10-15 21:48:23 -0400

    Remove misplaced files

--- Reverse_Words_in_a_String.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def reverseWords(self, s: str) -> str:
-        s = s.strip().split(" ")
-        res = []
-        for w in reversed(s):
-            if w:
-                res.append(w)
-        return " ".join(res)
